87ed2b788b5da9ea2f75aef4305b0637470d3770
தமிழ்நேரம் <anishprabu.t@gmail.com>
2021-10-19 14:00:17 +0530

    Temp files created for testing important APIs

--- TSM_tests/test_tsm_nidigital.py
+++ TSM_tests/test_tsm_nidigital.py
@@ -3,3 +3,21 @@ import src.nidevtools.digital
 
 
 @pytest.fixture
+def test():
+    pass
+
+
+"""The Following APIs/VIs are used in the DUT Power on sequence. 
+So these functions needs to be test first.
+
+# TSM SSC Digital Select Function.vi
+# TSM SSC Digital PPMU Source Voltage.vi
+# TSM SSC Digital Burst Pattern [Pass Fail].vi
+# TSM SSC Digital Apply Levels and Timing.vi
+# TSM SSC Digital Configure Time Set Period.vi
+# TSM SSC Digital Write Sequencer Register.vi
+# TSM SSC Digital Write Source Waveform [Broadcast].vi
+# TSM SSC Digital Write Static.vi
+# TSM SSC Digital N Pins To M Sessions.vi
+
+"""
