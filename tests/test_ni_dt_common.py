ffe433e853ffa06c84ae3ce4ef36d6b45b86892e
தமிழ்நேரம் <67970539+TamilNeram@users.noreply.github.com>
2021-10-20 05:16:21 -0500

    File names changed to avoid confusion between 1) Drivers (nidigital, dcpower, niscope etc...) 2) TSM APIs (tsm_func_names) i.e. files in nitsm-Devtools will have ni_dt_ as prefix. Updated test filenames

---
+++ tests/test_ni_dt_common.py
@@ -0,0 +1,21 @@
+import nidevtools.ni_dt_common as ni_dt_common
+import re
+
+# This file is used for testing some common functions
+
+input_data = "site0\\a, site0\\b,  site1\\c  ,site2\\d,site3/e, pin5"
+output_data = ni_dt_common.channel_list_to_pins(input_data)
+print(input_data)
+print(output_data)
+pattern = r"/\\"
+a = re.split(r'[/\\]', input_data)
+# print(a)
+
+data = []
+for pin_count in range(10):
+    pin_str = [""]
+    pins_array = pin_str * pin_count
+    # cluster = {pins_array}
+    data.append(pins_array)
+
+print(data)
