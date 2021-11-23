84b67214fb66ddd319942b98ad5c90ee460885f7
தமிழ்நேரம் <anishprabu.t@gmail.com>
2021-11-23 18:08:45 +0530

    Code sync-up.

--- tests/conftest.py
+++ tests/conftest.py
@@ -174,3 +174,12 @@ def test_pin_s(request):
     else:
         pins_selected = []
     return pins_selected
+
+
+@pytest.fixture
+def Simulated_Option(request):
+    """ To generate the simulated hardware option.
+    Need to revisit later"""
+    _, file_name = os.path.split(request.module.__file__)
+    OPTIONS = "Simulate = true, DriverSetup = Model : 5122"
+    return OPTIONS
--- tests/test_scope.py
+++ tests/test_scope.py
@@ -6,8 +6,67 @@ import nitsm.codemoduleapi
 from nitsm.codemoduleapi import SemiconductorModuleContext
 import nidevtools.scope as scope
 
+# To run the code on real hardware create a dummy file named "Hardware.exists" to flag SIMULATE_HARDWARE boolean.
+SIMULATE_HARDWARE = not os.path.exists(os.path.join(os.path.dirname(__file__), "Hardware.exists"))
+pin_file_names = ["simulated.pinmap", "scope.pinmap"]
+# Change index below to change the pinmap to use
+pin_file_name = pin_file_names[1]
+if SIMULATE_HARDWARE:
+    pin_file_name = pin_file_names[0]
+    pass
 
 OPTIONS = "Simulate = true, DriverSetup = Model : 5122"
+
+
+@pytest.fixture
+def tsm_context(standalone_tsm_context: SemiconductorModuleContext):
+    """
+    This TSM context is on simulated hardware or on real hardware based on OPTIONS defined below.
+    This TSM context uses standalone_tsm_context fixture created by the conftest.py
+    The fixture provides the digital project files necessary for initialisation of sessions
+    in a dictionary format.
+    """
+    print("\nTest is running on Simulated driver?", SIMULATE_HARDWARE)
+    if SIMULATE_HARDWARE:
+        # options = {"Simulate": True, "driver_setup": {"Model": "5122"}}
+        options = "Simulate = true, DriverSetup = Model : 5122"
+    else:
+        options = {}  # empty dict options to run on real hardware.
+
+    scope.tsm_scope_initialize_sessions(
+        standalone_tsm_context, options_input=options
+    )
+    yield standalone_tsm_context
+    scope.tsm_scope_close_sessions(standalone_tsm_context)
+
+
+@pytest.fixture
+def scope_tsm_s(tsm_context, test_pin_s):
+    """Returns LabVIEW Cluster equivalent data
+    This fixture accepts single pin in string format or
+    multiple pins in list of string format"""
+    scope_tsms = []
+    for test_pin in test_pin_s:
+        scope_tsms.append(scope.tsm_ssc_scope_pins_to_sessions(tsm_context, test_pin))
+    return scope_tsms
+
+
+@pytest.mark.pin_map(pin_file_name)
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+class TestNIScope:
+    """The Following APIs/VIs are used in the DUT Power on sequence.
+    So these functions needs to be test first.
+    """
+
+    def test_tsm_initialize_sessions(self, tsm_context):
+        """This Api is used in the Init routine"""
+        queried_sessions = list(tsm_context.get_all_niscope_sessions())
+        for session in queried_sessions:
+            assert isinstance(session, niscope.Session)
+        assert len(queried_sessions) == len(tsm_context.get_all_niscope_instrument_names())
+
+
+
 
 
 class SSCScope(typing.NamedTuple):
@@ -22,9 +81,9 @@ class TSMScope(typing.NamedTuple):
     ssc: typing.List[SSCScope]
 
 
-@pytest.mark.sequence_file("niscope.seq")
-def test_niscope(system_test_runner):
-    assert system_test_runner.run()
+# @pytest.mark.sequence_file("niscope.seq")
+# def test_niscope(system_test_runner):
+#     assert system_test_runner.run()
 
 
 @nitsm.codemoduleapi.code_module
