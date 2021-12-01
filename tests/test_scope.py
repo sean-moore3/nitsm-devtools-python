42d4ea9660171758b8657f8a26c9130dab1b9932
தமிழ்நேரம் <anishprabu.t@gmail.com>
2021-12-01 22:19:38 +0530

    Code sync-up.

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -87,32 +87,26 @@ class TestNIScope:
         for tsm_scope in scope_tsm_s:
             scope.configure_impedance(tsm_scope, 0.5)
             scope.configure_reference_level(tsm_scope)
-            # scope.configure_vertical(tsm_scope, 5.0, 0.0, niscope.VerticalCoupling.DC, 1.0, True)
             scope.configure(tsm_scope, 5.0, 1.0, 0.0, niscope.VerticalCoupling.DC, 10e6, 1000, 0.0, 0.0, 1e6, 1, True)
-            # scope.configure_vertical_per_channel(tsm_scope, 5.0, 0.0, niscope.VerticalCoupling.DC, 1.0, True)
             scope.configure_timing(tsm_scope, 20e6, 1000, 50, 1, True)
             self.subroutine_init_commit_abort(tsm_scope)
-            # scope.initiate(tsm_scope)
-            # scope.commit(tsm_scope)
-            # scope.abort(tsm_scope)
-            # scope.scope_get_session_properties(tsm_scope)
-            scope.scope_configure_digital_edge_trigger(
-                tsm_scope, "/OSC1/PXI_Trig0", niscope.TriggerSlope.POSITIVE)
-            scope.scope_configure_trigger(
-                tsm_scope, 0.0, niscope.TriggerCoupling.DC, niscope.TriggerSlope.POSITIVE)
+            # scope.scope_configure_digital_edge_trigger(tsm_scope, "/OSC1/PXI_Trig0", niscope.TriggerSlope.POSITIVE)
+            scope.scope_configure_trigger(tsm_scope, 0.0, niscope.TriggerCoupling.DC, niscope.TriggerSlope.POSITIVE)
             scope.tsm_ssc_scope_clear_triggers(tsm_scope)
             scope.tsm_ssc_scope_export_start_triggers(tsm_scope, scope.OUTPUT_TERMINAL.NONE)
             scope.tsm_ssc_scope_start_acquisition(tsm_scope)
             _, props=scope.scope_get_session_properties(tsm_scope)
-            print(props)
-            scope.scope_fetch_measurement(tsm_scope, niscope.ScalarMeasurement.NO_MEASUREMENT)
-            scope.scope_measure_statistics(tsm_scope, niscope.ScalarMeasurement.NO_MEASUREMENT)
+            print("\n",props)
+            _,measurement1=scope.scope_fetch_measurement(tsm_scope, niscope.ScalarMeasurement.VOLTAGE_PEAK_TO_PEAK)
+            print(measurement1)
+            _,measurement2=scope.scope_measure_statistics(tsm_scope, niscope.ScalarMeasurement.AVERAGE_PERIOD)
+            print(measurement2)
             scope.ssc_scope_fetch_clear_stats(tsm_scope.ssc)
             scope.tsm_ssc_scope_fetch_meas_stats_per_channel(
                 tsm_scope, niscope.ScalarMeasurement.NO_MEASUREMENT
             )
             _, data1, data2 = scope.scope_fetch_waveform(tsm_scope, 1)
-            print(data1, data2)
+            print(data1, data2,"\n")
             # print(scope.scope_fetch_multirecord_waveform(tsm_scope, 1))
 
 
