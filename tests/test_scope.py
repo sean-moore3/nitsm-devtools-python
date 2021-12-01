d9fe1493719d3637afc5c1808d1832dacb91577c
தமிழ்நேரம் <anishprabu.t@gmail.com>
2021-12-01 19:00:58 +0530

    Code sync-up.

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -67,7 +67,7 @@ class TestNIScope:
         for scope_tsm in scope_tsm_s:
             assert isinstance(scope_tsm, scope.TSMScope)
 
-    def test_configure_vertical(self, scope_tsm_s, test_pin_s, ):
+    def test_configure_vertical(self, scope_tsm_s):
         for tsm_scope in scope_tsm_s:
             scope.configure_impedance(tsm_scope, 0.5)
             scope.configure_reference_level(tsm_scope)
@@ -90,6 +90,60 @@ class TestNIScope:
                 tsm_scope, 5.0, 0.0, niscope.VerticalCoupling.DC, 1.0, True
             )
             scope.configure_timing(tsm_scope, 20e6, 1000, 50, 1, True)
+
+
+    def test_init_commit_abort(self, scope_tsm_s):
+        for tsm_scope in scope_tsm_s:
+            scope.initiate(tsm_scope)
+            scope.commit(tsm_scope)
+            scope.abort(tsm_scope)
+
+
+    def test_triggered_acquition(self,scope_tsm_s):
+        for tsm_scope in scope_tsm_s:
+            scope.configure_impedance(tsm_scope, 0.5)
+            scope.configure_reference_level(tsm_scope)
+            scope.configure_vertical(tsm_scope, 5.0, 0.0, niscope.VerticalCoupling.DC, 1.0, True)
+            scope.configure(
+                tsm_scope,
+                5.0,
+                1.0,
+                0.0,
+                niscope.VerticalCoupling.DC,
+                10e6,
+                1000,
+                0.0,
+                0.0,
+                1e6,
+                1,
+                True,
+            )
+            scope.configure_vertical_per_channel(
+                tsm_scope, 5.0, 0.0, niscope.VerticalCoupling.DC, 1.0, True
+            )
+            scope.configure_timing(tsm_scope, 20e6, 1000, 50, 1, True)
+            scope.initiate(tsm_scope)
+            scope.commit(tsm_scope)
+            scope.abort(tsm_scope)
+            scope.scope_get_session_properties(tsm_scope)
+            scope.scope_configure_digital_edge_trigger(
+                tsm_scope, "/OSC1/PXI_Trig0", niscope.TriggerSlope.POSITIVE)
+            scope.scope_configure_trigger(
+                tsm_scope, 0.0, niscope.TriggerCoupling.DC, niscope.TriggerSlope.POSITIVE)
+            scope.tsm_ssc_scope_clear_triggers(tsm_scope)
+            scope.tsm_ssc_scope_export_start_triggers(tsm_scope, scope.OUTPUT_TERMINAL.NONE)
+            scope.tsm_ssc_scope_start_acquisition(tsm_scope)
+            _, props=scope.scope_get_session_properties(tsm_scope)
+            print(props)
+            scope.scope_fetch_measurement(tsm_scope, niscope.ScalarMeasurement.NO_MEASUREMENT)
+            scope.scope_measure_statistics(tsm_scope, niscope.ScalarMeasurement.NO_MEASUREMENT)
+            scope.ssc_scope_fetch_clear_stats(tsm_scope.ssc)
+            scope.tsm_ssc_scope_fetch_meas_stats_per_channel(
+                tsm_scope, niscope.ScalarMeasurement.NO_MEASUREMENT
+            )
+            _, data1, data2 = scope.scope_fetch_waveform(tsm_scope, 1)
+            print(data1, data2)
+            # print(scope.scope_fetch_multirecord_waveform(tsm_scope, 1))
 
 
 class SSCScope(typing.NamedTuple):
