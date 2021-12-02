a8a96b7f23ec124a61e985d308109b8a30f5cdfd
TamilNeram <anishprabu.t@gmail.com>
2021-12-02 17:12:18 -0600

    clear_daqmx_task

--- src/nidevtools/_daqmx.py
+++ src/nidevtools/_daqmx.py
@@ -1,3 +1,14 @@
 import nidaqmx
+import nitsm.codemoduleapi
+from nitsm.codemoduleapi import SemiconductorModuleContext
 
-# your code
+@nitsm.codemoduleapi.code_module
+def clear_daqmx_task(tsm_context: SemiconductorModuleContext):
+    tasks_AI = tsm_context.get_all_daqmx_task()#GETALLNIDAQmxTASK
+    tasks_AO = tsm_context.get_all_daqmx_task()#GETALLNIDAQmxTASK
+    for task_AI in tasks_AI:
+        task_AI.stop()
+        task_AI.close()
+    for task_AO in tasks_AO:
+        task_AO.stop()
+        task_AO.close()
