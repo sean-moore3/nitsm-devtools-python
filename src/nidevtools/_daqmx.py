f121640dd50fd3b1ea1532294c3d37a42d307344
தமிழ்நேரம் <anishprabu.t@gmail.com>
2021-12-07 04:54:56 -0600

    DCPower bug fix

--- src/nidevtools/_daqmx.py
+++ src/nidevtools/_daqmx.py
@@ -2,6 +2,7 @@ import nidaqmx
 from nidaqmx.constants import TerminalConfiguration
 import nitsm.codemoduleapi
 from nitsm.codemoduleapi import SemiconductorModuleContext
+from nitsm.enums import InstrumentTypeIdConstants
 from enum import Enum
 import typing
 
@@ -43,23 +44,32 @@ def tsm_set_daqmx_task(tsm_context: SemiconductorModuleContext, input_voltage_ra
 def daqmx_instrument_type_id(instrument_type_id : str):
     return instrument_type_id
 
-#def daqmx_get_all_instrument_names(tsm_context: SemiconductorModuleContext, instrument_type_id):
+def daqmx_get_all_instrument_names(tsm_context: SemiconductorModuleContext, instrument_type_id: typing.Union[InstrumentTypeIdConstants ,str]): #did not match with labview entrances
+    instrument_names, channel_group_ids, channel_lists = tsm_context.get_custom_instrument_names(instrument_type_id)
+    return instrument_names, channel_group_ids, channel_lists
 
-#def get_all_sessions(tsm_context: SemiconductorModuleContext, instrument_type_id):
+def get_all_sessions(tsm_context: SemiconductorModuleContext, instrument_type_id: typing.Union[InstrumentTypeIdConstants ,str]): #did not match with labview entrances
+    daqmx_task , channel_group_ids , channel_lists = tsm_context.get_all_custom_sessions(instrument_type_id)
+    return daqmx_task
 
-#def daqmx_pins_to_session_sessions_info(tsm_context: SemiconductorModuleContext, pins)
-#    tsm_context.filter_pins_by_instrument_type(pins, nitsm.codemoduleapi.InstrumentTypeIdConstants.NI_DAQMX)
+def daqmx_pins_to_session_sessions_info(tsm_context: SemiconductorModuleContext, pins: typing.Sequence(str)):
+    pin_list = tsm_context.filter_pins_by_instrument_type(pins, nitsm.codemoduleapi.InstrumentTypeIdConstants.NI_DAQMX)
+    data_id = tsm_context.pins_to_custom_session(nitsm.codemoduleapi.InstrumentTypeIdConstants.NI_DAQMX, pin_list)
+    tsm_context.get_site_data(data_id)
 
-#def daqmx_pins_to_sessions_sessions(tsm_context: SemiconductorModuleContext, pins, instrument_type_id):
+def daqmx_pins_to_sessions_sessions(tsm_context: SemiconductorModuleContext, pins: typing.Union[InstrumentTypeIdConstants ,str], instrument_type_id: typing.Union[str, typing.Sequence[str]]):
+    pin_query_context, daqmx_task, channel_group_ids,  channel_lists = tsm_context.pins_to_custom_sessions(instrument_type_id, pins)
+    return pin_query_context, daqmx_task, channel_group_ids,  channel_lists
 
-#def daqmx_set_session(tsm_context: SemiconductorModuleContext, instrument_name, channel_group_id, instrument_type_id, daqmx_session):
 
+def daqmx_set_session(tsm_context: SemiconductorModuleContext, instrument_name: str, channel_group_id: str, instrument_type_id: str, daqmx_session):
+    tsm_context.set_custom_session(instrument_type_id, instrument_name, channel_group_id, daqmx_session)
 
 #Read
 @nitsm.codemoduleapi.code_module
 def daqmx_read_waveform_multichannel(daqmx_sessions):
     for daqmx_task in daqmx_sessions:
-        daqmx_task = nidaqmx.Task()
+        #daqmx_task = nidaqmx.Task()
         daqmx_task.read() # Review function
     return daqmx_sessions
 
