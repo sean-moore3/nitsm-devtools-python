1f8c0ece6364ebdd7a87ec199545fcc9075a700b
தமிழ்நேரம் <anishprabu.t@gmail.com>
2022-01-10 16:45:10 +0530

    code updated

--- tests/SPI_Custom/SPI_nidigital.py
+++ tests/SPI_Custom/SPI_nidigital.py
@@ -1,11 +1,18 @@
 import nitsm.codemoduleapi
 from nitsm.codemoduleapi import SemiconductorModuleContext
 import digital as ni_dt_digital
+import os.path
+import typing
 
+# To run the code on simulated hardware create a dummy file named "Simulate.driver" to flag SIMULATE_HARDWARE boolean.
+SIMULATE_HARDWARE = os.path.exists(os.path.join(os.path.dirname(__file__), "Simulate.driver"))
+OPTIONS = {}  # empty dict options to run on real hardware.
+if SIMULATE_HARDWARE:
+    OPTIONS = {"Simulate": True, "driver_setup": {"Model": "6571"}}
 
 @nitsm.codemoduleapi.code_module
 def initialize_sessions(tsm_context: SemiconductorModuleContext):
-    ni_dt_digital.tsm_initialize_sessions(tsm_context)
+    ni_dt_digital.tsm_initialize_sessions(tsm_context, options=OPTIONS)
     tsm_spi = ni_dt_digital.tsm_ssc_n_pins_to_m_sessions(
         tsm_context, ["CS", "SCK", "MOSI", "MISO", "SYNC"]
     )
@@ -15,29 +22,28 @@ def initialize_sessions(tsm_context: SemiconductorModuleContext):
 
 @nitsm.codemoduleapi.code_module
 def slave_select_via_sync(tsm_context: SemiconductorModuleContext):
-    tsm_sync = ni_dt_digital.tsm_ssc_n_pins_to_m_sessions(tsm_context, ["CS", "SCK", "SYNC"])
-    # ni_dt_digital.tsm_ssc_select_function(tsm_o, ni_dt_digital.enums.SelectedFunction.DIGITAL)
+    tsm_sync = ni_dt_digital.tsm_ssc_n_pins_to_m_sessions(tsm_context, ["SYNC"])
     ni_dt_digital.tsm_ssc_write_static(tsm_sync, ni_dt_digital.enums.WriteStaticPinState.ZERO)
-    _, per_site_pass = ni_dt_digital.tsm_ssc_burst_pattern_pass_fail(tsm_sync, "SPI_sync_write")
+    ni_dt_digital.tsm_ssc_write_static(tsm_sync, ni_dt_digital.enums.WriteStaticPinState.ONE)
+    ni_dt_digital.tsm_ssc_write_static(tsm_sync, ni_dt_digital.enums.WriteStaticPinState.ZERO)
 
-
-@nitsm.codemoduleapi.code_module
-def spi_write(tsm_context: SemiconductorModuleContext):
-    tsm = ni_dt_digital.tsm_ssc_n_pins_to_m_sessions(tsm_context, ["CS", "SCK", "MOSI", "MISO"])
-    _, per_site_pass = ni_dt_digital.tsm_ssc_burst_pattern_pass_fail(tsm, "SPI_write")
-    print(per_site_pass)
-    return per_site_pass
 
 
 @nitsm.codemoduleapi.code_module
-def spi_read(tsm_context: SemiconductorModuleContext):
+def spi_write(tsm_context: SemiconductorModuleContext, data: typing.List[int] = [12, 24, 36, 55, 96],):
     tsm = ni_dt_digital.tsm_ssc_n_pins_to_m_sessions(tsm_context, ["CS", "SCK", "MOSI", "MISO"])
-    # ni_dt_digital.tsm_ssc_select_function(tsm_i, ni_dt_digital.enums.SelectedFunction.DIGITAL)
-    _, data = ni_dt_digital.tsm_ssc_read_static(tsm)
-    _, per_site_pass = ni_dt_digital.tsm_ssc_burst_pattern_pass_fail(tsm, "SPI_read")
-    print(data)
-    print(per_site_pass)
-    return data
+    ni_dt_digital.tsm_ssc_write_source_waveform_broadcast(tsm, "source_buffer", data, )
+    ni_dt_digital.tsm_ssc_burst_pattern(tsm, "SPI_write")
+
+
+@nitsm.codemoduleapi.code_module
+def spi_read(tsm_context: SemiconductorModuleContext, data: typing.List[int] = [12, 24, 36, 55, 96],):
+    tsm = ni_dt_digital.tsm_ssc_n_pins_to_m_sessions(tsm_context, ["CS", "SCK", "MOSI", "MISO"])
+    ni_dt_digital.tsm_ssc_write_source_waveform_broadcast(tsm, "source_buffer", data, )
+    ni_dt_digital.tsm_ssc_burst_pattern(tsm, "SPI_read")
+    _, per_site_waveforms = ni_dt_digital.tsm_ssc_fetch_capture_waveform(tsm, "capture_buffer", 5)
+    print(per_site_waveforms)
+    return per_site_waveforms
 
 
 @nitsm.codemoduleapi.code_module
