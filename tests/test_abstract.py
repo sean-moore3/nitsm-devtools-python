73be24243e452c09f69da37903002f68bfb4ec33
தமிழ்நேரம் <anishprabu.t@gmail.com>
2022-03-09 16:47:01 -0600

    code syncup

--- tests/test_abstract.py
+++ tests/test_abstract.py
@@ -28,27 +28,11 @@ def tsm_context(standalone_tsm):
     print("\nSimulated driver?", SIMULATE)
     ni_daqmx.set_task(standalone_tsm)
     ni_fpga.initialize_sessions(standalone_tsm)
-    #ni_dt_digital.tsm_initialize_sessions(standalone_tsm)
+    # ni_dt_digital.tsm_initialize_sessions(standalone_tsm)
     yield standalone_tsm
-    #ni_dt_digital.tsm_close_sessions(standalone_tsm)
+    # ni_dt_digital.tsm_close_sessions(standalone_tsm)
     ni_fpga.close_sessions(standalone_tsm)
     ni_daqmx.clear_task(standalone_tsm)
-
-
-@pytest.fixture
-def abstract_tsm_s(tsm_context, tests_pins):
-    """Returns LabVIEW Cluster equivalent data"""
-    print(tests_pins)
-    abstract_tsm = []
-    sessions = []
-    for test_pin_group in tests_pins:
-        print(test_pin_group)
-        data = ni_abstract.pins_to_sessions_sessions_info(tsm_context, test_pin_group)
-        print('pass')
-        abstract_tsm.append(data)
-        sessions += data
-    print(sessions)
-    yield tsm_context, abstract_tsm
 
 
 @pytest.mark.pin_map(pin_file_name)
@@ -74,9 +58,10 @@ class TestAbstract:
         enabled.disconnect_sessions_info(tsm_context)
         # ni_abstract.disconnect_all(tsm_context)
         abst_session = ni_abstract.pins_to_sessions_sessions_info(tsm_context, 'BuckSGL_1')
+        print(abst_session)
         enabled.read_state(tsm_context)
         ni_abstract.pins_to_task_and_connect(tsm_context, ['En_Daq'], ['BuckSGL_3', 'BuckSGL_4'])
-        #ni_abstract.disconnect_all(tsm_context)
+        # ni_abstract.disconnect_all(tsm_context)
         ni_abstract.disconnect_pin(tsm_context, "BuckSGL_1")
 
     def test_pin_name_to_instrument(self, tsm_context):
@@ -88,3 +73,55 @@ class TestAbstract:
         ni_abstract.pin_fgv(tsm_context, '', ni_abstract.Control.get_connections)
         print('DISCONNECT ALL')
         ni_abstract.pin_fgv(tsm_context, '', ni_abstract.Control.disconnect_all)
+
+
+def ts_open_session(tsm_context):
+    ni_daqmx.set_task(tsm_context)
+    ni_fpga.initialize_sessions(tsm_context)
+    # ni_dt_digital.tsm_initialize_sessions(tsm_context)
+
+
+def ts_close_session(tsm_context):
+    # ni_dt_digital.tsm_close_sessions(tsm_context)
+    ni_fpga.close_sessions(tsm_context)
+    ni_daqmx.clear_task(tsm_context)
+
+
+def ts_initialize_and_close(tsm_context):
+    ni_abstract.initialize_tsm_context(tsm_context)
+    assert(4 == len(ni_abstract.get_all_sessions(tsm_context).enable_pins))
+    assert(ni_abstract.get_all_instruments_names(tsm_context)[0] == 'Masterconnect')
+    ni_abstract.close_sessions(tsm_context)
+
+
+def ts_check_debug():
+    ni_abstract.check_debug_ui_tool("")  # TODO add path
+
+
+def ts_pins_to_session_sessions_info(tsm_context):
+    ni_abstract.initialize_tsm_context(tsm_context)
+    pins = ['En_Daq']
+    enabled = ni_abstract.enable_pins_to_sessions(tsm_context, pins)
+    assert(enabled.enable_pins[0].enable_pin == pins[0])
+    enabled.connect_sessions_info(tsm_context)
+    t = ni_daqmx.get_all_sessions(tsm_context)[0]
+    t.stop()
+    enabled.disconnect_sessions_info(tsm_context)
+    # ni_abstract.disconnect_all(tsm_context)
+    abst_session = ni_abstract.pins_to_sessions_sessions_info(tsm_context, 'BuckSGL_1')
+    print(abst_session)
+    enabled.read_state(tsm_context)
+    ni_abstract.pins_to_task_and_connect(tsm_context, ['En_Daq'], ['BuckSGL_3', 'BuckSGL_4'])
+    # ni_abstract.disconnect_all(tsm_context)
+    ni_abstract.disconnect_pin(tsm_context, "BuckSGL_1")
+
+
+def ts_pin_name_to_instrument(tsm_context):
+    # ni_abstract.pin_name_to_instrument(pinmap_path='C:\\Users\\ni\\Desktop\\Baku_uSTS.pinmap')
+    # print(tsm_context.pin_map_file_path)
+    print('INIT')
+    ni_abstract.pin_fgv(tsm_context, '', ni_abstract.Control.init)
+    print('GET CONNECTIONS')
+    ni_abstract.pin_fgv(tsm_context, '', ni_abstract.Control.get_connections)
+    print('DISCONNECT ALL')
+    ni_abstract.pin_fgv(tsm_context, '', ni_abstract.Control.disconnect_all)
