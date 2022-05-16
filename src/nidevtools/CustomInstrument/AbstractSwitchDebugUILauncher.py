a05464ecb7e78a24a4f5d28e511e4ca1e49e4c5a
தமிழ்நேரம் <anishprabu.t@gmail.com>
2022-05-16 14:14:33 -0500

    updated the Init for testing

--- src/nidevtools/CustomInstrument/AbstractSwitchDebugUILauncher.py
+++ src/nidevtools/CustomInstrument/AbstractSwitchDebugUILauncher.py
@@ -2,15 +2,17 @@ import sys
 
 import nitsm.codemoduleapi
 from nitsm.codemoduleapi import SemiconductorModuleContext as SMContext
+
 from PyQt5 import QtWidgets
+
 from nidevtools import UI_Debug_AbstractSwitch
 
 
-# @nitsm.codemoduleapi.code_module
-# def run_ui(tsm: SMContext):
-def run_ui():
+@nitsm.codemoduleapi.code_module
+def run_ui(tsm: SMContext):
     app = QtWidgets.QApplication(sys.argv)
-    # UI_Debug_AbstractSwitch.tsm_context = tsm
+
+    UI_Debug_AbstractSwitch.tsm_context = tsm
     # AbstractSwitchDebugWindow = UI_Debug_AbstractSwitch.MainWindow(tsm)
     AbstractSwitchDebugWindow = UI_Debug_AbstractSwitch.MainWindow()
     ui_abstract_switch_debug_window = UI_Debug_AbstractSwitch.UiAbstractSwitchDebugWindow()
