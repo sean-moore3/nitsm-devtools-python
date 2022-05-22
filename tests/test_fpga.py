3b3ea8c2a9b00f9ad7c2757e03ef11973aefeb62
தமிழ்நேரம் <anishprabu.t@gmail.com>
2022-05-21 22:28:04 -0500

    code update

--- tests/test_fpga.py
+++ tests/test_fpga.py
@@ -6,12 +6,12 @@ import os
 from nitsm.codemoduleapi import SemiconductorModuleContext as SMContext
 import nidevtools.fpga as ni_fpga
 
-# To run the code on simulated hardware create a dummy file named "Simulate.driver" to flag SIMULATE boolean.
+# To simulate hardware create a dummy file named "Simulate.driver" to flag SIMULATE boolean.
 SIMULATE = os.path.exists(os.path.join(os.path.dirname(__file__), "Simulate.driver"))
 
-pin_file_names = ["MonoLithic.pinmap", "Rainbow.pinmap"]
+pin_file_names = ["MonoLithic.pinmap", "FPGA.pinmap"]
 # Change index below to change the pin map to use
-pin_file_name = pin_file_names[0]
+pin_file_name = pin_file_names[1]
 
 OPTIONS = {}  # empty options to run on real hardware.
 if SIMULATE:
